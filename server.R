ab21d3973307459d8fd519c3c39af0b5c685dd51
Shu Wan <shu.wan@outlook.com>
2016-07-26 15:55:33 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,97 @@
+library(dplyr)
+library(data.table)
+library(leaflet)
+library(shiny)
+library(shinydashboard)
+library(dygraphs)
+library(xts)
+
+# read data
+data <- tbl_df(fread('Washington_DC.csv',header = T))
+# modify data
+data <- data %>%
+  mutate(incident_date = as.Date(data$incident_datetime, format = "%m/%d/%Y")) %>%
+  mutate(incidentType = as.factor(parent_incident_type)) %>%
+  mutate(content = paste('<b>',incidentType,'</b>')) %>%
+  mutate(content = paste(sep = '<br/>', content,incident_datetime,address_1)) %>%
+  mutate(year_month = as.yearmon(incident_date))
+
+# Customize icons
+crimeIcons <- iconList(
+  Robbery = makeIcon(iconUrl = './icon/Robbery.png',iconWidth = 35,iconHeight = 35),
+  `Theft of Vehicle` = makeIcon(iconUrl = './icon/Theft of Vehicle.png', iconWidth = 35, iconHeight = 35),
+  `Theft from Vehicle` = makeIcon(iconUrl = './icon/Theft from Vehicle.png', iconWidth = 35, iconHeight = 35),
+  `Breaking & Entering` = makeIcon(iconUrl = './icon/Breaking & Entering.png', iconWidth = 35, iconHeight = 35),
+  Theft = makeIcon(iconUrl = './icon/Theft.png', iconWidth = 35, iconHeight = 35),
+  `Sexual Assault` = makeIcon(iconUrl = './icon/Sexual Assault.png', iconWidth = 35, iconHeight = 35),
+  `Assault with Deadly Weapon` = makeIcon(iconUrl = './icon/Assault with Deadly Weapon.png', iconWidth = 35, iconHeight = 35),
+  Homicide = makeIcon(iconUrl ='./icon/Homicide.png' ,iconWidth =35 ,iconHeight = 35),
+  Arson = makeIcon(iconUrl ='./icon/Arson.png' ,iconWidth = 35 ,iconHeight = 35)
+)
+
+
+server <- function(input, output) {
+  filteredData <- reactive({
+    data %>%
+      filter(parent_incident_type %in% input$crimeType ) %>%
+      filter(incident_date > input$dates[1] & incident_date < input$dates[2]) %>%
+      filter(day_of_week %in% input$day_of_week) %>%
+      filter(hour_of_day >= input$time_of_day & hour_of_day <= input$time_of_day)
+  })
+    
+  output$crimeMap <- renderLeaflet({
+   leaflet(filteredData()) %>%
+      addTiles(group = 'OSM') %>%
+      addProviderTiles('Esri.WorldStreetMap', group = 'Esri') %>%
+      addProviderTiles('CartoDB.Positron', group = 'CartoDB') %>%
+      addMarkers(
+        ~longitude, ~latitude, popup = ~content, 
+        icon = ~crimeIcons[incidentType],clusterOptions = markerClusterOptions()
+      ) %>%
+      addLayersControl(
+        baseGroups = c('OSM', 'Esri', 'CartoDB'),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  
+  
+  output$plot1 <- renderDygraph({
+    dydf <- matrix()
+    for (i in 1:length(input$crimeType)){
+      temp <- filteredData() %>%
+        filter(incidentType %in% input$crimeType[i]) %>%
+        group_by(year_month) %>%
+        summarise(count = n()) %>%
+        arrange(year_month)
+      temp1 <- xts(temp$count, temp$year_month)
+      if(dim(temp1)[[1]] == 0) {
+        temp1 <- 0
+      }
+      dydf <- cbind(dydf, temp1)
+    }
+    dydf <- dydf[,-1]
+    dydf[is.na(dydf)] <- 0
+    colnames(dydf) <- input$crimeType
+    dygraph(dydf, main = 'Incidents Trend/Month') %>%
+      dyOptions(colors = RColorBrewer::brewer.pal(9, "Set1"),
+                drawPoints = TRUE, pointSize = 2) %>%
+      dyHighlight(highlightCircleSize = 5, 
+                  highlightSeriesBackgroundAlpha = 0.2,
+                  hideOnMouseOut = FALSE,
+                  highlightSeriesOpts = list(strokeWidth = 3)) %>%
+      dyAxis("y", label = 'Count') %>%
+      dyRangeSelector(fillColor = '#651365')
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,70 @@
+library(leaflet)
+library(shiny)
+library(shinydashboard)
+library(dygraphs)
+
+type <- c("Robbery" = "Robbery", "Theft of Vehicle"= "Theft of Vehicle",
+  "Theft from Vehicle" =  "Theft from Vehicle", "Breaking & Entering" = "Breaking & Entering",
+  "Theft" = "Theft", "Assault with Deadly Weapon" = "Assault with Deadly Weapon",
+  "Sexual Assault" =  "Sexual Assault", "Homicide" = "Homicide","Arson" = "Arson")
+header <- dashboardHeader(title = p("Crime Around You",tags$sup('TM')),
+                          titleWidth = 400)
+
+
+body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
+            fluidRow(
+              column(width =4,
+                     box(width = NULL, title =tagList(shiny::icon("filter",class = 'fa-lg'), "Filter Data") ,
+                         solidHeader = T, collapsible = T, status = 'primary',
+                         selectizeInput('crimeType','Crime Type', choices = type, width = 380,
+                                        selected = c('Robbery', 'Theft of Vehicle',"Theft from Vehicle",
+                                                     "Breaking & Entering","Theft","Assault with Deadly Weapon",
+                                                     "Sexual Assault","Homicide","Arson"),multiple = T),
+                         dateRangeInput('dates', label = "Date Range",width = 380,
+                                        start = '2014-01-01', end = '2015-01-01',
+                                        min = "2013-01-01", max = "2016-08-01"
+                         ),
+                         selectizeInput('day_of_week','Days of Week', width = 380,
+                                        choices = c('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'),
+                                        selected = c('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'),
+                                        multiple = T),
+                         sliderInput('time_of_day','Time of Day', min = 0, max = 23,width = 380,
+                                     value = c(0,23), step = 1),
+                         submitButton(text = "Submit",icon =icon('filter'))
+                         ),
+                     box(width = NULL,title = tagList(shiny::icon("info-circle",class = 'fa-lg'), "About Crime Around You"), solidHeader = T, collapsible = T, status = 'info',
+                         strong("Crime Around You"),"is an interactive map built on shiny which allows you to cutomize
+                         time range and incident type to filter out crime reports of a certain location you want. The",em("dygraph"),
+                         "below the map allows you to explore incidents trend over period of time. The data is from", a('here.', href = 'https://moto.data.socrata.com/dataset/Washington-DC/2vfk-6rp4', target = "_blank")
+                         ),
+                     box(width = NULL,
+                         icon('github', class = 'fa-lg'), a('Source Code', href = 'https://github.com/Shu-Wan/crimearoundyou', target = "_blank"),
+                         br(),
+                         br(),
+                         icon('github-alt', class = 'fa-lg'), a('My Github Page', href = 'https://github.com/Shu-Wan/', target = "_blank"),
+                         br(),
+                         br(),
+                         icon('linkedin-square', class = 'fa-lg'), a('My Linkedin Page', href = 'https://www.linkedin.com/in/shu-wan-88153493', target = "_blank"))
+                     ),
+              column(width =8,
+                     box(width = NULL, solidHeader = TRUE,
+                         leafletOutput('crimeMap',height = 500)),
+                     box(width = NULL, 
+                         dygraphOutput('plot1')
+              )
+            )
+            )
+            )
+
+                      
+ui <- dashboardPage(skin = 'purple',
+                    header,
+                    dashboardSidebar(disable = T),
+                    body
+                    )
+
+
+
+
